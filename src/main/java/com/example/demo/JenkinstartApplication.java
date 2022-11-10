4a660a055422e090401c95fc3975793e0e50e1d7
unknown <khambalkarlavish@gmail.com>
2022-11-10 17:34:32 +0530

    We will done tomorrow

---
+++ src/main/java/com/example/demo/JenkinstartApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinstartApplication.class, args);
+	}
+
+}
